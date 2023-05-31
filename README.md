e9b0f068774aae927532d6d36bc691c90e234d64
Sintel <sintel.inquiries@gmail.com>
2023-05-31 12:54:11 +0200

    add doc comments

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 ![build](https://github.com/Sin-tel/no_denormals/actions/workflows/rust.yml/badge.svg)
 # no_denormals
-scoped control for turning off denormal floating point numbers
+Scoped control for turning off denormal floating point numbers.
--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,13 @@
-// RAII-style guard guard for turning off floating point denormals
+//! Temporarily turn off floating point denormals.
+//!
+//! Internally, this uses a RAII-style guard to manage the state of certain processor flags.
+//! On x86 and x86_64, this sets the flush-to-zero and denormals-are-zero flags in the MXCSR register.
+//! On aarch64 this sets the flush-to-zero flag in the FPCR register.
+//! In all cases, the register will be reset to its initial state when the guard is dropped.
 
-use core::cell::Cell;
+#![warn(missing_docs, missing_debug_implementations, rust_2018_idioms)]
+
 use core::marker::PhantomData;
-use std::sync::MutexGuard;
 
 #[cfg(not(any(target_arch = "x86", target_arch = "x86_64", target_arch = "aarch64")))]
 compile_error!("This crate only supports x86, x86_64 and aarch64.");
@@ -15,20 +20,17 @@ const X86_MASK: u32 = 0x8040;
 #[cfg(target_arch = "aarch64")]
 const AARCH64_MASK: u64 = 1 << 24;
 
-// These processor flags are local to each thread.
-// We implement !Send and !Sync with this workaround,
-// because negative trait bounds are not yet supported.
-type PhantomUnsync = PhantomData<Cell<()>>;
-type PhantomUnsend = PhantomData<MutexGuard<'static, ()>>;
-
-pub struct DenormalGuard {
-	_unsync: PhantomUnsync,
-	_unsend: PhantomUnsend,
-
+struct DenormalGuard {
 	#[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
 	mxcsr: u32,
 	#[cfg(target_arch = "aarch64")]
 	fpcr: u64,
+
+	// These processor flags are local to each thread.
+	// We implement !Send and !Sync with this workaround,
+	// because negative trait bounds are not yet supported.
+	// https://users.rust-lang.org/t/negative-trait-bounds-are-not-yet-fully-implemented-use-marker-types-for-now/64495/2
+	_not_send_sync: PhantomData<*const ()>,
 }
 
 impl DenormalGuard {
@@ -46,8 +48,7 @@ impl DenormalGuard {
 
 			return DenormalGuard {
 				mxcsr,
-				_unsync: PhantomData,
-				_unsend: PhantomData,
+				_not_send_sync: PhantomData,
 			};
 		}
 		#[cfg(target_arch = "aarch64")]
@@ -58,8 +59,7 @@ impl DenormalGuard {
 
 			return DenormalGuard {
 				fpcr,
-				_unsync: PhantomData,
-				_unsend: PhantomData,
+				_not_send_sync: PhantomData,
 			};
 		}
 	}
@@ -84,6 +84,7 @@ impl Drop for DenormalGuard {
 	}
 }
 
+/// Calls the `func` closure.
 pub fn no_denormals<T, F: FnOnce() -> T>(func: F) -> T {
 	let guard = DenormalGuard::new();
 	let ret = func();
